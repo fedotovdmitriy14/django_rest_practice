5d935de73d71e8cbd24032384587cc108b22ffe0
fedotovdmitriy14 <fedotovdmitriy14@gmail>
2021-10-25 15:54:57 +0300

    add searching ordering filtering

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -7,11 +7,29 @@ from store.serializers import BookSerializer
 
 
 class BooksApiTestCase(APITestCase):
+    def setUp(self):
+        self.book1 = Book.objects.create(name='test_book1', price=25, author_name='Author 1')
+        self.book2 = Book.objects.create(name='test_book2', price=45, author_name='Author 5')
+        self.book3 = Book.objects.create(name='test_book Author 1', price=65, author_name='Author 2')
+
     def test_get(self):
-        book1 = Book.objects.create(name='test_book1', price=25)
-        book2 = Book.objects.create(name='test_book2', price=45)
         url = reverse('book-list')
         response = self.client.get(url)
-        serializer_data = BookSerializer([book1, book2], many=True).data
+        serializer_data = BookSerializer([self.book1, self.book2, self.book3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
+
+    def test_filter(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'search': 'Author 1'})                    # тест для поиска
+        serializer_data = BookSerializer([self.book1, self.book3], many=True).data      # в сериалайзер приходят только два объекта
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_ordering(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'ordering': '-price'})
+        serializer_data = BookSerializer([self.book3, self.book2, self.book1], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
